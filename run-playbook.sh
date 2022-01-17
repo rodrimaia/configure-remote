c15f1424a5eb0832ea14fdc51187529f01b41b48
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2022-01-17 13:25:22 +0000

    :coffee:

--- run-playbook.sh
+++ run-playbook.sh
@@ -2,23 +2,6 @@
 
 set -e
 
-function usage {
-  echo "Usage: run-playbook.sh -u USER -r HOST"
-}
+ansible-galaxy install -r requirements.yml
 
-unset USERNAME REMOTE_HOST
-
-while getopts 'u:r:' c
-do
-  case $c in
-    u) USERNAME=$OPTARG ;;
-    r) REMOTE_HOST=$OPTARG ;;
-  esac
-done
-
-if [[ -z "$USERNAME" || -z "$REMOTE_HOST" ]]; then
-  usage
-  exit 1
-fi
-
-ansible-playbook -i $USERNAME@$REMOTE_HOST, playbook.yml
+ansible-playbook playbook.yml
